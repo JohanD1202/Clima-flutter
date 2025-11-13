d3dfc8d4bc0a381001578cd78a387ebeb2a91d62
Johan <idarragajohan8@gmail.com>
2025-11-12 21:15:02 -0500

    Commit inicial - Configuración de la aplicación meteorológica de Flutter

---
+++ lib/domain/entities/weather.dart
@@ -0,0 +1,21 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String description;
+  final double feelsLike;
+  final int humidity;
+  final double windSpeed;
+  final String main;
+  final DateTime date;
+
+  const Weather({
+    required this.cityName,
+    required this.temperature,
+    required this.description,
+    required this.feelsLike,
+    required this.humidity,
+    required this.windSpeed,
+    required this.main,
+    required this.date,
+  });
+}
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'weather/home_screen.dart';
+
